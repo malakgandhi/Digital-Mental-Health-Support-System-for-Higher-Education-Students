ed155101fcab1dd58d3f9c8714933aef04b1d452
malakg <malakgandhi05@gmail.com>
2026-02-20 21:29:32 +0530

    Made changes.

--- analysis.py
+++ analysis.py
@@ -1,3 +1,8 @@
 from pandas import read_csv
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import roc_auc_score, auc, roc_curve
+
+df = read_csv("Depression Student Dataset.csv")
+
+print(df.isnull().sum())
